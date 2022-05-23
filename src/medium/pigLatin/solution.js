405c2b80e2c99808d0fcce8d204e4382dc6c4509
Hubert Lemczak <hubertlemczak@gmail.com>
2022-05-23 04:47:40 +0100

    c

--- src/medium/pigLatin/solution.js
+++ src/medium/pigLatin/solution.js
@@ -2,16 +2,14 @@ const pigLatin = (sentence) => {
   const translatedSentence = [];
   const words = sentence.split(' ');
   for (let i = 0; i < words.length; i++) {
-    if (/([A-Z])/.test(words[i][0])) {
+    if (/([A-Z])/.test(words[i][0]))
       translatedSentence.push(
         words[i].slice(1, 2).toUpperCase() +
           words[i].slice(2) +
           words[i].slice(0, 1).toLowerCase() +
           'ay'
       );
-    } else {
-      translatedSentence.push(words[i].slice(1) + words[i].slice(0, 1) + 'ay');
-    }
+    else translatedSentence.push(words[i].slice(1) + words[i].slice(0, 1) + 'ay');
   }
   return translatedSentence.join(' ');
 };
